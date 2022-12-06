b06ffbcf05bc78cbe78156373187156feaeedf07
Samson Okaludo <samson@divercity.io>
2022-12-06 17:01:28 +0100

    feat: initial commit

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+
+// middleware
+app.use(express.json()); // lets us have our data in req.body
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task manager");
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is running on port ${port}...`));
